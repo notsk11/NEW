e8adb665827ecd745384ad6e73badca9e2e67e2d
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-04-08 12:06:21 +0530

    Create style.py

---
+++ modules/style.py
@@ -0,0 +1,79 @@
+css = """
+div.tab-nav.scroll-hide.svelte-1uw5tnk {
+  left: 1310px;
+  top: -1080px;
+  width: 560px;
+  position: absolute;
+}
+.tab {
+  left: 1310px;
+  top: -1050px;
+  width: 560px;
+  position: absolute;
+}
+.app.svelte-182fdeq.svelte-182fdeq {
+    position: fixed;
+    margin: auto;
+    padding: var(--size-4) var(--size-8);
+    width: 100%;
+    height: 100%;
+}
+.gradio-container {
+    left: -20px !important;
+    top: -20px !important;
+    display: flex;
+    overflow: visible;
+}
+.image {
+  left: 0px;
+  top: 0px;
+  width: 1300px;
+  height: 700px;
+}
+.prompt {
+  width: 600px;
+  height: 65px;
+}
+.neg-prompt {
+  width: 600px;
+  height: 65px;
+}
+.height {
+  width: 400px;
+  top: -150px;
+  left: 620px;
+}
+.height-mark {
+  top: -215px;
+  left: 620px;
+}
+.width {
+  width: 400px;
+  top: -190px;
+  left: 620px;
+}
+.width-mark {
+  top: -250px;
+  left: 620px;
+}
+.gen {
+  width: 250px;
+  height: 70px;
+  top: 800px;
+  left: 1030px;
+  position: absolute;
+}
+.model {
+  padding-top: 0px;
+  padding-right: 10px;
+  padding-bottom: 0px;
+  padding-left: 10px;
+  height: 70px;
+}
+.details {
+  padding: 0px;
+}
+.load {
+  padding: 5px;
+}
+"""
