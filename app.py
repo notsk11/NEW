46db0c950cdd8b3cc8c156a30a90735d80d9532c
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-04-19 00:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,74 @@
 # /content/app.py
 
 import gradio as gr
-import numpy as np
-from PIL import Image
-from diffusers import DiffusionPipeline
-import torch
-from modules import txt2img
-from modules.txt2img import txt2img
-from modules import pipeline
-from modules.pipeline import load_pipeline_global
 from modules import style
 from modules.style import css
-from modules.scheduler import update_scheduler  # Import update_scheduler from scheduler module
-from modules.scheduler import scheduler_constructors
-from diffusers import (
-    PNDMScheduler,
-    DEISMultistepScheduler,
-    UniPCMultistepScheduler,
-    EulerDiscreteScheduler,
-    EulerAncestralDiscreteScheduler,
-    LMSDiscreteScheduler,
-    KDPM2DiscreteScheduler,
-    KDPM2AncestralDiscreteScheduler,
-    DPMSolverSinglestepScheduler,
-    DPMSolverMultistepScheduler,
-)
-
-
-# Rest of your app code remains the same
-
-
+from modules import txt2img
+from modules.txt2img import txt2img
+from modules.pipeline import load_pipeline
+from modules.pipeline import update_scheduler
 
 with gr.Blocks(css=style.css) as demo:
-  with gr.Tab("Txt2Img", elem_classes="tab-t2i"):
-    with gr.Group():
+  gr.Markdown("Stable Diffusion Checkpoint", elem_classes="stable-diff")
+  with gr.Row():
+    model_global = gr.Textbox(elem_classes="model", container=False, value="SG161222/Realistic_Vision_V6.0_B1_noVAE")
+    load_model_global = gr.Button(value="Load", elem_classes="load-model")
+  with gr.Tab("Txt2Img", elem_classes="tab"):
+    with gr.Row():
       with gr.Column():
+        prompt_t2i = gr.Textbox(elem_classes="prompt", container=False, lines=3, placeholder="Place Promps here....")
+        negative_prompt_t2i = gr.Textbox(elem_classes="negative-prompt", container=False, lines=3, placeholder="Place Negative Promps here....")
+      with gr.Column():
+        generate_t2i = gr.Button(value="Generate", elem_classes="generate-t2i")
+    with gr.Tab("Generation", elem_classes="tab2"):
+      with gr.Row():
         with gr.Column():
-          prompt_t2i = gr.Textbox(elem_classes="prompt-t2i", lines=3, container=False, placeholder="Place Prompts here....")
-          negative_prompt_t2i = gr.Textbox(elem_classes="negative-prompt-t2i", lines=3, container=False, placeholder="Place Negative Prompts here....")
-        with gr.Row():
-          seed_inp = gr.Textbox(elem_classes="seed-input-t2i", lines=1, container=False, placeholder="Place Seed here....")
-        with gr.Row():
-          height_t2i = gr.Slider(elem_classes="height-t2i", label="Height", minimum=408, maximum=1600, value=408, step=8)
-          width_t2i = gr.Slider(elem_classes="width-t2i", label="Width", minimum=408, maximum=1600, value=408, step=8)
-        with gr.Row():
-          num_inference_steps_t2i = gr.Slider(elem_classes="num-steps-t2i", label="Sampling Steps", minimum=1, maximum=100, value=20, step=1, scale=8)
-          guidance_scale_t2i = gr.Slider(elem_classes="guidance-scale-t2i", label="CFG Scale", minimum=0, maximum=10, value=7.5, step=0.1, scale=5)
-        with gr.Row():
-          batch_size_t2i = gr.Slider(elem_classes="batch-size-t2i", label="Batch Size", minimum=1, maximum=10, value=1, step=1)
-      with gr.Column():
-        with gr.Row():
-          model_global = gr.Textbox(elem_classes="model_global", label="Model", value="SG161222/Realistic_Vision_V6.0_B1_noVAE", scale=5)
-          load_model_global = gr.Button("Load Model", elem_classes="load-model-gloabl", scale=1)
-      with gr.Column():
-        with gr.Row():
-          scheduler = gr.Dropdown(elem_classes="scheduler", choices=list(scheduler_constructors.keys()), label="Sampling Method", value="DPM-SDE-Karras", scale=5)
-          load_scheduler = gr.Button("Load Scheduler", scale=1)
+          gr.Markdown("Sampling Methods", elem_classes="samp-meth-mark")
+          scheduler = gr.Dropdown(elem_classes="samp-meth", choices=[
+          "PNDM",
+          "DEIS",
+          "UniPC",
+          "Euler",
+          "Euler-A",
+          "LMS",
+          "LMS-Karras",
+          "DPM2",
+          "DPM2-Karras",
+          "DPM-SDE",
+          "DPM-SDE-Karras",
+          "DPM-2M",
+          "DPM-2M-Karras",
+          "DPM2-A",
+          "DPM2-A-Karras",
+          "DPM-2M-SDE",
+          "DPM-2M-SDE-Karras"], value="DPM-SDE-Karras", container=False)
         with gr.Column():
-          generate_t2i = gr.Button("Generate", elem_classes="generate-t2i", scale=3)
-    image_out_t2i = gr.Gallery(elem_classes="image-out-t2i")
-    metadata_t2i = gr.Textbox(elem_classes="metadata-t2i", lines=5, container=False, placeholder="Data about generated image here....")
-  with gr.Tab("Img2Img", elem_classes="tab-t2i"):
-    lol = gr.Textbox()
-    load_model_global.click(fn=load_pipeline_global, inputs=[model_global])
-    load_scheduler.click(fn=update_scheduler, inputs=[scheduler])
-    generate_t2i.click(fn=txt2img, inputs=[prompt_t2i, negative_prompt_t2i, height_t2i, width_t2i, num_inference_steps_t2i, guidance_scale_t2i, batch_size_t2i, seed_inp], outputs=[image_out_t2i, metadata_t2i])
+          load_scheduler_t2i = gr.Button(value="Load", elem_classes="load-scheduler-t2i")
+        with gr.Column():
+          gr.Markdown("Sampling Steps", elem_classes="samp-steps-mark")
+          num_inference_steps_t2i = gr.Slider(elem_classes="samp-steps", minimum=1, maximum=100, value=10, step=1, container=False)
+        with gr.Column():
+          image_out_t2i = gr.Gallery(elem_classes="image-output-t2i")
+      with gr.Row():
+        with gr.Column():
+          gr.Markdown("Height", elem_classes="height-t2i-mark")
+          height_t2i = gr.Slider(elem_classes="height-t2i", minimum=100, maximum=1600, value=408, step=4, container=False)
+          gr.Markdown("Width", elem_classes="width-t2i-mark")
+          width_t2i = gr.Slider(elem_classes="width-t2i", minimum=100, maximum=1600, value=408, step=4, container=False)
+        with gr.Column():
+          gr.Markdown("Batch Count", elem_classes="batch-count-t2i-mark")
+          batch_count_t2i = gr.Slider(elem_classes="batch-count-t2i", minimum=1, maximum=10, step=1, value=1, container=False)
+          gr.Markdown("Batch Size", elem_classes="batch-size-t2i-mark")
+          batch_size = gr.Slider(elem_classes="batch-size-t2i", minimum=1, maximum=10, value=1, step=1, container=False)
+      with gr.Column():
+        gr.Markdown("CFG Scale", elem_classes="guidance-scale-t2i-mark")
+        guidance_scale_t2i = gr.Slider(elem_classes="guidance-scale-t2i", minimum=0, maximum=10, value=7.5, step=0.1, container=False)
+        gr.Markdown("Seed", elem_classes="seed-input-t2i-mark")
+        seed_input_t2i = gr.Textbox(elem_classes="seed-input-t2i", container=False)
+        metadata_t2i = gr.Textbox(elem_classes="metadata-t2i", container=False, lines=10, show_copy_button=True)
+      load_model_global.click(fn=load_pipeline, inputs=[model_global])
+      load_scheduler_t2i.click(fn=update_scheduler, inputs=[scheduler])
+      generate_t2i.click(fn=txt2img, inputs=[prompt_t2i, negative_prompt_t2i, height_t2i, width_t2i, num_inference_steps_t2i, guidance_scale_t2i, batch_count_t2i, seed_input_t2i], outputs=[image_out_t2i, metadata_t2i])
+
+
 demo.launch(share=True, debug=True)
